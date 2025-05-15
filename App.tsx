3557e1e092c4cdfeba0fe7d2a50d22e9d7fa65bc
Amir Husain <amir.developer786@outlook.com>
2025-05-15 14:33:50 +0530

    Updated code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
 import AppNavigation from 'navigation/appNavigation';
 import { GlobalProvider } from 'context/GlobalProvider';
 
 export default function App() {
+  console.log('App.tsx');
   return (
     <>
       {/* <ScreenContent title="Home" path="App.tsx"></ScreenContent>
